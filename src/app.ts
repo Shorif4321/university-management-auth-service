bab12ff321657b073fb46f59e0be6715c6a58566
unknown <shorifali4321@gmail.com>
2024-05-23 13:09:26 +0600

    prettier,huskey

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,10 @@
-import express, { Application, Request, Response, urlencoded } from "express"
+import express, { Application, Request, Response, urlencoded } from 'express'
 import cors from 'cors'
 
 const app: Application = express()
-const port: Number = 3000
-
 
 // middleware
-app.use(cors());
+app.use(cors())
 
 // parser
 app.use(express.json())
@@ -14,7 +12,7 @@ app.use(urlencoded({ extended: true }))
 
 // testing
 app.get('/', (req: Request, res: Response) => {
-    res.send('working successfully')
+  res.send('working successfully')
 })
 
-export default app;
+export default app
