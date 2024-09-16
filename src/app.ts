8a13fd77e131ae9ff6b6b3250055c892a390d045
Shorif4321 <shorifali4321@gmail.com>
2024-09-16 20:03:41 +0600

    all fix

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
-const app:Application = express()
+const app: Application = express()
 
 // midleware
-       app.use(cors())
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
