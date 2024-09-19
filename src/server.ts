4c11e88cf2f6f1b73ce69abeebb3c6439dadc64e
Shorif4321 <shorifali4321@gmail.com>
2024-09-19 19:28:30 +0600

    eror fix

--- src/server.ts
+++ src/server.ts
@@ -3,22 +3,47 @@ import mongoose from 'mongoose'
 import config from './config/index'
 import app from './app'
 import { logger, errorLogger } from './shared/logger'
+import { Server } from 'http'
 
-// const port = 3000
+// uncought Exception
+process.on('uncaughtException', error => {
+  errorLogger.error(error)
+  process.exit(1)
+})
 
-// 3F3a04iO6WLe0d8F  university-admin
+let server: Server
 
 async function bootstrap() {
   try {
     await mongoose.connect(config.database_Url as string)
     logger.info(`🛢 Database is Connected Successfully`)
 
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       logger.info(`Application listening on port ${config.port}`)
     })
   } catch (err) {
     errorLogger.error('Failed To to database', err)
   }
+
+  // server gracefully of
+  process.on('unhandledRejection', error => {
+    if (server) {
+      server.close(() => {
+        errorLogger.error(error)
+        process.exit(1) // node process of after server OF
+      })
+    } else {
+      process.exit(1)
+    }
+  })
 }
 
 bootstrap()
+
+// sigteam setup
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM is received')
+  if (server) {
+    server.close()
+  }
+})
