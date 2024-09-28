65a94266c1145504772b9a3932475f292fe2f7ad
Shorif4321 <shorifali4321@gmail.com>
2024-09-28 14:24:30 +0600

    academic semsester compleate

--- presentation.js
+++ presentation.js
@@ -14,7 +14,6 @@
     next();
   }); */ // again postman hit =  {{UMA}}/lalalala
 
-
 //   14-2: What is pagination , Create Semester Controller
 // academicSemester.controller.ts file write getAllSemester functino +
 // ekta object paginamtionOptions + result = copy code from upper functin + export getAll Funtion +
@@ -34,15 +33,12 @@
 // const paginamtionOptions = pick(req.query,paginationField) + import + log + hit in postman = {{UMA}}/academic-semesters/?page=1&limit=10
 // output in teminal = { page: '1', limit: '10' }
 
-
-
 // 14-4: Implement Pagination using page , limit and skip
 // remove code that is comment and upcommetn result code  from AS.controller.ts
 // go to academicSemeterService.ts = distrucring page and limit from paginationOption
 // user page and limit diteo pare nao pare so set defult number 1 and 10
 // skipp banate hbe + model a questy korte hbe + getAllSemester Return korbe ekta <IAcadimcSemester>
 // retun asbe ekta formate a return { meta:{page,limit, total },data:result} ei data type ta ektu vinno , so etar jonne ekta data type banabo  IGenericResponse =
-
 
 // 14-5: Implement Dynamic Sorting
 //pagination sob page use korbo , student, facalti, admin sobar jonnei
@@ -55,14 +51,11 @@
 // received sortBy and sortOrder in academicSemester.service.ts by distructuring and make a object set key and value in object + use the object in sort()
 // {{UMA}}/academic-semesters/?sortBy=code&&sortOrder=desc   {{UMA}}/academic-semesters/?sortBy=year&&sortOrder=asc
 
-
-
 // 14-6: What is searching and filtering
 // search partial match  , filter exact matched
 // make const filters = pick(req.query,['searchTerm])
 // {{UMA}}/academic-semesters/?searchTerm=Autum
 // academicSemster.service.ts = filters: IAcademicSemesterFilters, make type for filter IAcademicSemesterFilters = cut and paste in academicSemester.interface.ts + export
-
 
 // 14-7: Implement Dynamic Searching
 // searchTerm distructuring from academicSemester.service.ts  + make vaiable andCondionts ={$or[{}]} ==see the code  + call andCondition in find({andConditions})
@@ -70,7 +63,6 @@
 // again search implement on "code"
 // check /?searchTerm=03 + search implement on "year" = check /?searchTerm=2026
 // andCondionts ekta repeted task so amra search filed ke akta array baniye tar opre map kore use korlei hbe == see academicSemester.service.ts + {{UMA}}/academic-semesters/?searchTerm=autu
-
 
 // 14-8: Implement Dynamic Filtering
 // add value in const filters = pick(req.query, ['searchTerm', 'title', 'year', 'code']);
@@ -88,7 +80,6 @@
 // code optimaize =    const academicSemestersSearchableFields = ['title', 'code', 'year']; + paste in  academicSemester.constant.ts file + export + import in service.ts file +
 // copy  ['searchTerm', 'title', 'year', 'code'] + paset in constant.ts + export + import
 
-
 // 14-9: Fix an error and create single semester
 // {{UMA}}/academic-semesters hit korle error dibe  = AcademicSemester.find()  find empy kore dile kaj krob
 //searching ba parination thakteo pare nao pare so,, eita optional banate hbe, i mean pagination ba search ba filter charao api load korte hbe
@@ -98,14 +89,11 @@
 // router.get('/:id', AcademicSemesterController.getSingleSemester) // create getSIngleSemeste in controller.ts file // must above the router.get('/) + getSingleSemetes in contorler do code see the upper fucnton + getSineleSemester in service.ts + do code see upper code funcitn
 // solve error  + {{UMA}}/academic-semesters/66f4215d2f42a3d0d11cadd1
 
-
 // 14-10: Handle Cast Error
 // {{UMA}}/academic-semesters/66f4215d2f42a3d0d11cadd1  eivabe hit korle amara data pacci but jodi id er last number chage kroe hist kori tbe data pabo na == abar id theke koyekta number remover kore hit korele cust error dibe {{UMA}}/academic-semesters/66f4215d2f42a3d0d1
 // globalErrorHandelar.ts = CastError with if condition + res.status(200).json({error}) = hist with single id
 // make handleCastError for simplyfied  in Error Folter = handleCastError.ts == see
 // check by hit on wrong id
-
-
 
 // 14-11: Update Academic Semester
 //in academicSemester.route.ts file make app.patch('/',academicSemesterController.updateSemester) = above the get route
@@ -120,22 +108,18 @@
 //   "year":"2031",
 //   "code":"01"
 // }
-// code=01 hole ager create kroar validation ke overwrite kortece , so etakeo solve korte hbe  
+// code=01 hole ager create kroar validation ke overwrite kortece , so etakeo solve korte hbe
 
-
-
-
-
-  //   "title":"Fall",
-  //   "year":"2031",
-  //   "code":"01"
-  // }
-  // code=01 hole ager create kroar validation ke overwrite kortece , so etakeo solve korte hbe  
+//   "title":"Fall",
+//   "year":"2031",
+//   "code":"01"
+// }
+// code=01 hole ager create kroar validation ke overwrite kortece , so etakeo solve korte hbe
 // 14-12: Implement validation on update
 // Ensure: Route Level: update ==> give me title and code both , neither
 // Ensure2: Service Level: Update ==> mapping title:code
 // make updateAcademicSemesterZodSchema in academicSemesterValidation.ts file = see ===>
-  // .optional() call korte hbe .. ta na hole sob ooption theke error dibe
+// .optional() call korte hbe .. ta na hole sob ooption theke error dibe
 // in route call validationRequest and give updateAcademicSemesterZodSchema this as value
 // validateRequest give error for type = solve it in validateRequest.ts file  = (schema: AnyZodObject | ZodEffects<AnyZodObject>) =>
 // {{UMA}}/academic-semesters/66f421652f42a3d0d11cadd4 = ekta property diye hit korle error dibe
@@ -145,31 +129,25 @@
 
 // {
 //   "title":"Fall",
-//   "code":"02", 
+//   "code":"02",
 // } // duita dile kar kobe but fall er jonne code je 03 hote hbe setake chek krotece na, ei problem ta servie layer a korte hbe
 
-// academicSemesterService.ts = 
+// academicSemesterService.ts =
 // if (academicSemesterTitleCodeMapper[payload.title] !== payload.code) {
 //   throw new ApiError(httpStatus.BAD_REQUEST, 'Invalid Semester Code');
 // } // copy from create academicSemeste and paste in update academic semster with payload.title && payload.code && condition
 //   "title":"Fall",
-//   "code":"02", 
+//   "code":"02",
 // } // with the value hit postman update {{UMA}}/academic-semesters/id , ekta propery dile route lavel validation check korbe ar wrong code dile service theke validation korbe
-
 
 // 14-13: Practice and Create Academic faculty Module
 // Error: Cannot set headers after they are sent to the client === in terminal see this
 // coder moode response pathanor pore jodi kon ekta kaj korteco ba next kon ekta middleware ke call korteco, sei middleware abar tomar header ta set korar try kortece, tokhon tomar kace ei eror ta ase ,= tumi to client ke ekbar header pathiye dico tai na ? er por tumi abar kano set korba, so error dicce
 // academicSemester.controller.ts a sendResponse er pore next() ke call deya ace , tar mane response ekbar client ke deyar pore abar next() ke call dicce -- response er pore ar next() dorkar nai
-// remove next() + next:Nextfunction from academicSemester.controller.ts and user.controller.ts + globalErrorHandelar.ts 
+// remove next() + next:Nextfunction from academicSemester.controller.ts and user.controller.ts + globalErrorHandelar.ts
 // amar sob kaj ses korar pore response pathai , response er proe kon kaj ar hbe na
 // update api + hit korle ar error dibe na
 
-// delete korbo 
+// delete korbo
 // create route in route.delete('/',academicSemesterController.deleteSemester) + acacdemicSemester.controller.ts + acacdemicSemester.serive.ts
-// postman his delete by id   
-
-
-
-
-
+// postman his delete by id
