3cabeb4876156511072cf70730a7595fbb738871
shorif4321 <shorifali4321@gmail.com>
2024-10-02 09:29:53 +0600

    create student id

--- presentation.js
+++ presentation.js
@@ -1,2 +1,11 @@
 // 15-1: Overview of User Roles , Create Enums
-// creae enums folder in src,  user.ts + export enum ENUM_USER_ROLE{ SUPER_ADMIN='super_admin',ADMIN='admin',STUDENT='student',FACULTY='faculty'} 
+// creae enums folder in src,  user.ts + export enum ENUM_USER_ROLE{ SUPER_ADMIN='super_admin',ADMIN='admin',STUDENT='student',FACULTY='faculty'}
+// user.utils.ts file = genarateUserId = genarateStudentId
+
+
+// 15-2: Create generateStudentId() utility and test it
+// chage findLastUserId to findLastStudentId + let to const incrementalId
+// ager incremental id er sathe year ar last dui digit + code and ager id set kore incrementalId er moodei update korteci =  incrementalId = `${academicSemester.year.substring(2)}${academicSemester.code}${incrementalId}`   + console.log(incrementalId);
+// test kore dekhar jonne app.ts a const acdemicSemester = {code:'01',year:'2025'}
+// create function = const testId = async()=>{const testId = await generateStudentId(academicSemester) console.log(testId) } + call the function
+
--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import globalErrorHandelar from './middlewares/globalErrorHandelers';
 import routers from './app/routes';
 import httpStatus from 'http-status';
+import { generateStudentId } from './app/modules/user/user.utils';
 
 const app: Application = express();
 
@@ -15,8 +16,6 @@ app.use(express.urlencoded({ extended: true }));
 // app.use('/api/v1/users', UserRoutes);
 // app.use('/api/v1/academic-semesters', AcademicSemesterRoutes);
 app.use('/api/v1/', routers);
-
-export default app;
 
 app.use(globalErrorHandelar);
 
@@ -41,3 +40,16 @@ app.get('/', async () => {
 app.get('/', async (req: Request, res: Response) => {
   res.send('Working Successfully');
 });
+
+const academicSemester = {
+  code: '01',
+  year: '2025',
+};
+const testId = async () => {
+  const testId = await generateStudentId(academicSemester);
+  console.log(testId);
+};
+
+testId();
+
+export default app;
