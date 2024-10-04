ff4e668aed41bc15403b67fc7f2e735c9376fab8
Shorif4321 <shorifali4321@gmail.com>
2024-10-04 17:08:28 +0600

    create student update route

---
+++ src/app/modules/student/student.controller.ts
@@ -0,0 +1,70 @@
+import { Request, Response } from 'express';
+import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import httpStatus from 'http-status';
+import pick from '../../../shared/pick';
+import { paginationFields } from '../../../constants/pagination';
+import { IStudent } from './student.interface';
+import { studentFilterableFields } from './student.constant';
+import { StudentService } from './student.service';
+
+const getAllStudents = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, studentFilterableFields);
+  const paginamtionOptions = pick(req.query, paginationFields);
+  const result = await StudentService.getAllStudents(
+    filters,
+    paginamtionOptions,
+  );
+
+  sendResponse<IStudent[]>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Semester Retrieved Successfully!',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
+const getSingleStudent = catchAsync(async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const result = await StudentService.getSingleStudent(id);
+
+  sendResponse<IStudent>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Semester Retrieve Successfully!',
+    data: result,
+  });
+});
+
+const updateStudent = catchAsync(async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const updatedData = req.body;
+  const result = await StudentService.updateStudent(id, updatedData);
+
+  sendResponse<IStudent>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Semester updated Successfully!',
+    data: result,
+  });
+});
+
+const deleteStudent = catchAsync(async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const result = await StudentService.deleteStudent(id);
+
+  sendResponse<IStudent>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Semester deleted Successfully!',
+    data: result,
+  });
+});
+
+export const StudentController = {
+  getAllStudents,
+  getSingleStudent,
+  updateStudent,
+  deleteStudent,
+};
